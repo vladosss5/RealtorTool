89e4db567092ff2f6f694d181725a92feb4b7b6b
Кипоров Владислав <KiporovVA@tomskasu.ru>
2025-10-23 00:53:28 +0700

    Добавил Hot Reload

--- RealtorTool.Desktop/App.axaml.cs
+++ RealtorTool.Desktop/App.axaml.cs
@@ -2,11 +2,19 @@ using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Microsoft.Extensions.DependencyInjection;
 using RealtorTool.Desktop.Views.Windows;
+using HotAvalonia;
 
 namespace RealtorTool.Desktop;
 
 public partial class App : Application
 {
+    public override void Initialize()
+    {
+        this.EnableHotReload();
+        
+        base.Initialize();
+    }
+
     public override void OnFrameworkInitializationCompleted()
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
